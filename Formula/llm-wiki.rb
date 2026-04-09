60473e53d19558a22233ac9ea484d9c6bf82f235
Lon Lundgren <4402378+lcdupree@users.noreply.github.com>
2026-04-09 10:26:23 -0700

    Create llm-wiki.rb

---
+++ Formula/llm-wiki.rb
@@ -0,0 +1,18 @@
+class LlmWiki < Formula
+  desc "Placeholder for the LLM-Wiki project"
+  homepage "https://wiki-llm.org"
+  
+  url "https://raw.githubusercontent.com/github/choosealicense.com/gh-pages/_licenses/mit.txt"
+  version "0.0.1"
+  sha256 "e6919db13cacb3300f918d1048d996d0f4506e1f110b7caa21f31ea7ca3ef5b8"
+
+  def install
+    # This is a placeholder install step
+    touch "placeholder"
+    bin.install "placeholder" => "wiki-llm"
+  end
+
+  test do
+    system "true"
+  end
+end
